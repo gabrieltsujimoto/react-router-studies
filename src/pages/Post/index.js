a9aaf55d3145cba4111cfafc492e5150e4b97495
GabTVicente <gabrieltsujimotovicente@outlook.com>
2023-01-10 14:01:23 -0300

    Modify

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -3,9 +3,6 @@ import { useParams } from 'react-router-dom'
 
 export default function Post() {
     const params = useParams();
-
-
-    
     return (
     <div>Post Renderizado foi o: {params.id}</div>
   )
